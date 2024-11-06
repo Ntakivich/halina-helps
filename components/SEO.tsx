130124a413d05ad37ea646b7d9cd018c9cd0cf5f
Ntakivich <ntakivich@gmail.com>
2024-11-06 21:39:32 +0100

    added google meta

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -11,6 +11,7 @@ function SEO() {
       <meta name="description" content={seoData.description} />
       <meta name="keywords" content={seoData.keywords.join(", ")} />
       <link rel="canonical" href={seoData.url} />
+      <meta name="google-site-verification" content="r8ENPbXpGiVgE-0PfBSgWN7FpeKDeezq8K0mEFjszPw" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={seoData.url} />
       <meta property="og:title" content={seoData.title} />
